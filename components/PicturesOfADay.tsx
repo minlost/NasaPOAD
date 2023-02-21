97cec5e32e4a3dc9ea68f4c4d4bc57f13e014330
minlost <97989349+minlost@users.noreply.github.com>
2023-02-21 17:58:26 +0100

    third

--- components/PicturesOfADay.tsx
+++ components/PicturesOfADay.tsx
@@ -30,7 +30,7 @@ function PicturesOfADay({ arr }: picProps) {
   const [modUrl, setModUrl] = useState("")
   const [indexImg, setIndexImg] = useState<number>(0)
   const [isPlaying, setIsPlaying] = useState(false)
-  const [audio] = useState(new Audio("../34723.mp3"))
+  // const [audio] = useState(new Audio("/34723.mp3"))
 
   function handleClick(tit: string) {
     setTitles(tit)
@@ -41,15 +41,15 @@ function PicturesOfADay({ arr }: picProps) {
     setIndexImg(idx)
   }
 
-  function playMusic() {
-    if (!isPlaying) {
-      audio.play()
-      setIsPlaying(true)
-    } else {
-      audio.pause()
-      setIsPlaying(false)
-    }
-  }
+  // function playMusic() {
+  //   if (!isPlaying) {
+  //     audio.play()
+  //     setIsPlaying(true)
+  //   } else {
+  //     audio.pause()
+  //     setIsPlaying(false)
+  //   }
+  // }
 
   function handleMove(direction: string) {
     let ixNum = indexImg
@@ -72,14 +72,14 @@ function PicturesOfADay({ arr }: picProps) {
   return (
     <>
       <FaPlay
-        onClick={() => playMusic()}
+        // onClick={() => playMusic()}
         className={`absolute top-[40px] right-[50px] text-[25px] ${
           isPlaying ? "opacity-90" : "opacity-50"
         } text-white cursor-pointer hover:opacity-80`}
       />
 
       <AiOutlinePause
-        onClick={() => playMusic()}
+        // onClick={() => playMusic()}
         className={`absolute top-[35px] right-[15px] text-[35px] ${
           isPlaying ? "opacity-50" : "opacity-90"
         } text-white cursor-pointer hover:opacity-80`}
